cf302285f4e4a172f83ee9ae011c9e2d1679691e
imronkyu2 <example@email.comimron.kyu2@gmail.com>
2022-08-29 17:38:30 +0700

    Part 1 - Setup & Search User

--- app/src/main/java/com/example/learngithubuserapp/ui/main/MainActivity.kt
+++ app/src/main/java/com/example/learngithubuserapp/ui/main/MainActivity.kt
@@ -2,15 +2,41 @@ package com.example.learngithubuserapp.ui.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.KeyEvent
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.learngithubuserapp.R
 import com.example.learngithubuserapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding :  ActivityMainBinding
+    private lateinit var viewModel: MainViewModel
+    private lateinit var adapter: UserAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        adapter = UserAdapter()
+        adapter.notifyDataSetChanged()
+        viewModel = ViewModelProvider(this, ViewModelProvider.NewInstanceFactory()).get(MainViewModel::class.java)
+
+        binding.apply {
+            rvUser.layoutManager = LinearLayoutManager(this@MainActivity)
+            rvUser.setHasFixedSize(true)
+            rvUser.adapter = adapter
+
+            btnSearch.setOnClickListener{
+
+            }
+
+            etQuery.setOnKeyListener{v, keyCode, event ->
+                if (event.action == KeyEvent.ACTION_DOWN && keyCode == KeyEvent.ACTION_DOWN){
+                    return@setOnKeyListener true
+                }
+                return@setOnKeyListener false
+            }
+        }
     }
 }
--- app/src/main/java/com/example/learngithubuserapp/ui/main/MainViewModel.kt
+++ app/src/main/java/com/example/learngithubuserapp/ui/main/MainViewModel.kt
@@ -1,4 +1,41 @@
 package com.example.learngithubuserapp.ui.main
 
-class MainViewModel {
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.example.learngithubuserapp.api.RetrofitClient
+import com.example.learngithubuserapp.data.model.User
+import com.example.learngithubuserapp.data.model.UserResponse
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
+class MainViewModel : ViewModel() {
+
+    val listUser = MutableLiveData<ArrayList<User>>()
+
+    fun setSearchUser(query: String){
+        RetrofitClient.apiInstance
+            .getSearchUser(query)
+            .enqueue(object :Callback<UserResponse>{
+                override fun onResponse(
+                    call: Call<UserResponse>,
+                    response: Response<UserResponse>
+                ) {
+                    if (response.isSuccessful){
+                        listUser.postValue(response.body()?.items)
+                    }
+                }
+
+                override fun onFailure(call: Call<UserResponse>, t: Throwable) {
+                    Log.d("failure", t.message.toString());
+                }
+
+            })
+    }
+
+    fun getSearchUser():LiveData<ArrayList<User>>{
+        return listUser
+    }
 }
